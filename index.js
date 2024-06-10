5bba3252e614183053ff46ca340ede4e190e4e59
ahmad <ahmad.ben.hamad2004@gmail.com>
2024-06-10 04:20:54 +0100

    implemented edit label

--- index.js
+++ index.js
@@ -66,13 +66,11 @@ app.post('/login', async (c) => {
 app.post('/pull', async (c) => {
     console.log("pulling")
     const { username } = await c.req.json();
-    console.log(username)
     const query = {
         text: 'SELECT * FROM public."item" WHERE user_name = $1',
         values: [username],
     };
     const res = await client.query(query);
-    console.log(res.rows)
     return c.json({ list: res.rows })
 
 })
@@ -80,34 +78,39 @@ app.post('/push', async (c) => {
     console.log("pushing")
     const { list, username } = await c.req.json();
     console.log(username)
+    let query = { text: "", values: [] }
     list.forEach(async (element) => {
-        console.log(element)
-        console.log(element.type)
+
         if (element.type === 'add') {
-            let query = {
+            query = {
                 text: 'INSERT INTO public.item (label, user_name, state) VALUES ($1, $2, $3) ',
                 values: [element.label, username, element.info.state],
             };
-            const res = await client.query(query);
         }
         else if (element.type === 'edit') {
+
             if (element.info.collum === 'state') {
-                let query = {
+                query = {
                     text: 'UPDATE public.item SET state = $1 WHERE label = $2 AND user_name = $3 ',
-                    values: [element.info.value,element.label,username],
+                    values: [element.info.value, element.label, username],
                 };
-                const res = await client.query(query);
             }
-            // edit on the label to be implemented !!! 🤓
+
+            else if (element.info.collum === 'label') {
+                query = {
+                    text: 'UPDATE public.item SET label = $1 WHERE label = $2 AND user_name = $3 ',
+                    values: [element.info.value, element.label, username],
+                };
+            }
 
         }
         else if (element.type === 'remove') {
-            let query = {
+            query = {
                 text: 'DELETE FROM public.item WHERE label = $1 AND user_name = $2;',
                 values: [element.label, username],
             }
-            const res = await client.query(query);
         }
+        const res = await client.query(query);
     });
     return c.json({ message: true })
 })
@@ -116,10 +119,10 @@ app.post('/push', async (c) => {
 
 app.post('/hardpush', async (c) => {
     const { list, username } = await c.req.json();
-    list.forEach( async (element) => {
+    list.forEach(async (element) => {
         let query = {
             text: 'INSERT INTO public.item(label, user_name, state) VALUES ($1, $2, $3) ON CONFLICT (user_name,label) DO UPDATE SET state = $3',
-            values: [element.label, username,element.state],
+            values: [element.label, username, element.state],
         }
         const res = await client.query(query);
     });
